855f13959c05b1b551f75b45905346714c68c095
むいにゃん <hngrmi@gmail.com>
2019-05-12 00:33:03 +0900

    diverta2019 A-D AC

---
+++ diverta_2019_Programming_Contest/A.py
@@ -0,0 +1,13 @@
+def s(generator, splitter, mapper):
+    return [ mapper(s) for s in generator().split(splitter) ]
+
+# スペース区切りの入力を読み込んで数値リストにして返します。
+def get_nums_l():
+    return [ int(s) for s in input().split(" ")]
+
+# 改行区切りの入力をn行読み込んで数値リストにして返します。
+def get_nums_n(n):
+    return [ int(input()) for _ in range(n)]
+
+n,k = get_nums_l()
+print(n-k+1)
