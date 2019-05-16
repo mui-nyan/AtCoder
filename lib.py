d50c7480735fadbbae0b5ba9904d7043e7621e35
むいにゃん <hngrmi@gmail.com>
2019-05-17 01:40:21 +0900

    テンプレ

---
+++ lib.py
@@ -0,0 +1,11 @@
+
+def gcd(m, n):
+    # 入力を m, n (m ≧ n) とする。
+    tmp = max(m,n)
+    n = min(m,n)
+    m = tmp
+    # n = 0 なら、 m を出力してアルゴリズムを終了する。
+    if n == 0:
+        return m
+    # m を n で割った余りを新たに n とし、更に 元のnを新たにm とし 2. に戻る。
+    return gcd(n, m % n)
