38d4eebea03f7f6cd0d770343281c55c8356739a
むいにゃん <hngrmi@gmail.com>
2019-06-09 19:14:25 +0900

    m-solのC,D(未完)

---
+++ corp/msol2019/C.py
@@ -0,0 +1,17 @@
+import math
+from functools import reduce
+from collections import deque
+
+def s(generator, splitter, mapper):
+    return [ mapper(s) for s in generator().split(splitter) ]
+
+# スペース区切りの入力を読み込んで数値リストにして返します。
+def get_nums_l():
+    return [ int(s) for s in input().split(" ")]
+
+# 改行区切りの入力をn行読み込んで数値リストにして返します。
+def get_nums_n(n):
+    return [ int(input()) for _ in range(n)]
+
+MOD = 10**9 + 7
+n,a,b,c = get_nums_l()
