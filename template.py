2285128bd8c71eedb320a2bf2c6620e28b4a3aa5
むいにゃん <hngrmi@gmail.com>
2020-02-03 21:56:59 +0900

    テンプレート改訂

--- template.py
+++ template.py
@@ -4,9 +4,6 @@ from collections import deque
 import sys
 sys.setrecursionlimit(10**7)
 
-def s(generator, splitter, mapper):
-    return [ mapper(s) for s in generator().split(splitter) ]
-
 # スペース区切りの入力を読み込んで数値リストにして返します。
 def get_nums_l():
     return [ int(s) for s in input().split(" ")]
@@ -14,5 +11,8 @@ def get_nums_l():
 # 改行区切りの入力をn行読み込んで数値リストにして返します。
 def get_nums_n(n):
     return [ int(input()) for _ in range(n)]
+
+def log(*args):
+    print("DEBUG:", *args, file=sys.stderr)
 
 lines = sys.stdin.readlines()
