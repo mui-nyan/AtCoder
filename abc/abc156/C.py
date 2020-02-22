dd102f6593f5e71ff87c850a85260c313c4b0111
むいにゃん <hngrmi@gmail.com>
2020-02-23 01:57:12 +0900

    ABC156 A-E

---
+++ abc/abc156/C.py
@@ -0,0 +1,31 @@
+import math
+from functools import reduce
+from collections import deque
+import sys
+sys.setrecursionlimit(10**7)
+
+# スペース区切りの入力を読み込んで数値リストにして返します。
+def get_nums_l():
+    return [ int(s) for s in input().split(" ")]
+
+# 改行区切りの入力をn行読み込んで数値リストにして返します。
+def get_nums_n(n):
+    return [ int(input()) for _ in range(n)]
+
+# 改行またはスペース区切りの入力をすべて読み込んでイテレータを返します。
+def get_all_int():
+    return map(int, open(0).read().split())
+
+def log(*args):
+    print("DEBUG:", *args, file=sys.stderr)
+
+
+n, = get_nums_l()
+xxx = get_nums_l()
+
+ans = 9999999999999999999999999
+for p in range(1,101):
+    a = sum([ (p-x)**2 for x in xxx ])
+    ans = min(ans, a)
+
+print(ans)
