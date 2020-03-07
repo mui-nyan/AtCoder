ad781ef429284bbb8ee7599cbf7f1318aca43295
むいにゃん <hngrmi@gmail.com>
2020-03-07 20:18:37 +0900

    セグメント木のライブラリ

--- lib.py
+++ lib.py
@@ -138,6 +138,46 @@ def neighbors4(x, y):
   for i in range(4):
     yield(x + dxy[i], y + dxy[i+1])
 
+
+class segtree:
+    def __init__(self, n,operator,identity):
+        nb = bin(n)[2:]
+        bc = sum([int(digit) for digit in nb])
+        if bc == 1:
+            self.num_end_leaves = 2**(len(nb)-1)
+        else:
+            self.num_end_leaves = 2**(len(nb))
+
+        self.array = [identity for i in range(self.num_end_leaves * 2)]
+        self.identity = identity
+        self.operator =operator
+
+    def update(self,x,val):
+        actual_x = x+self.num_end_leaves
+        self.array[actual_x] = val
+        while actual_x > 0 :
+            actual_x = actual_x//2
+            self.array[actual_x] = self.operator(self.array[actual_x*2],self.array[actual_x*2+1])
+
+    def get(self,q_left,q_right,arr_ind=1,leaf_left=0,depth=0):
+        """
+        [q_left, q_right] (閉区間) の値を集計して返します。
+        """
+
+        width_of_floor = self.num_end_leaves//(2**depth)
+        leaf_right = leaf_left+width_of_floor-1
+
+        if leaf_left > q_right or leaf_right < q_left:
+            return  self.identity
+
+        elif leaf_left >= q_left and leaf_right <= q_right:
+            return self.array[arr_ind]
+
+        else:
+            val_l = self.get(q_left,q_right,2*arr_ind,leaf_left,depth+1)
+            val_r = self.get(q_left,q_right,2*arr_ind+1,leaf_left+width_of_floor//2,depth+1)
+            return self.operator(val_l,val_r)
+
 ##
 # Union-Find
 nodes = 99999999999
