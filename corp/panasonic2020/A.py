e9629e3f7db8f17ab8dc8ee09ee243a95ca56391
むいにゃん <hngrmi@gmail.com>
2020-03-14 23:42:46 +0900

    panasonic2020 A-D

---
+++ corp/panasonic2020/A.py
@@ -0,0 +1,26 @@
+import math
+from functools import reduce
+from collections import deque
+import sys
+sys.setrecursionlimit(10**7)
+
+# スペース区切りの入力を読み込んで数値リストにして返します。
+def get_nums_l():
+    return [ int(s) for s in input().split(" ")]
+
+# 改行区切りの入力をn行読み込んで数値リストにして返します。
+def get_nums_n(n):
+    return [ int(input()) for _ in range(n)]
+
+# 改行またはスペース区切りの入力をすべて読み込んでイテレータを返します。
+def get_all_int():
+    return map(int, open(0).read().split())
+
+def log(*args):
+    print("DEBUG:", *args, file=sys.stderr)
+
+arr = [1, 1, 1, 2, 1, 2, 1, 5, 2, 2, 1, 5, 1, 2, 1, 14, 1, 5, 1, 5, 2, 2, 1, 15, 2, 2, 5, 4, 1, 4, 1, 51]
+
+k = int(input())
+
+print(arr[k-1])
