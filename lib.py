2f7113553d5f986e44498bbe050eda892149b80a
むいにゃん <hngrmi@gmail.com>
2020-05-31 23:25:59 +0900

    素因数分解のバグ修正

--- lib.py
+++ lib.py
@@ -13,16 +13,34 @@ def gcd(m, n):
 def lcm(m, n):
     return m*n//gcd(m,n)
 
-def factorization(n):
-    arr = []
-    temp = n
-    for i in range(2, int(-(-n**0.5//1))+1):
-        if temp%i==0:
-            cnt=0
-            while temp%i==0:
-                cnt+=1
-                temp //= i
-            arr.append([i, cnt])
+def prime_factorize(n):
+    """
+    素因数分解
+    
+    Sample:
+        prime_factorize(36)
+        => [2, 2, 3, 3]
+
+        collections.Counter(prime_factorize(36))
+        => {2:2, 3:2}
+    """
+    a = []
+    while n % 2 == 0:
+        a.append(2)
+        n //= 2
+    f = 3
+    while f * f <= n:
+        if n % f == 0:
+            a.append(f)
+            n //= f
+        else:
+            f += 2
+    if n != 1:
+        a.append(n)
+    return a
+
+    if len(arr) == 0:
+        arr.append((n, 1))
     return arr
 
 def fac(N):
