0417b6129aef87f916f57c54b59170a410335be4
むいにゃん <hngrmi@gmail.com>
2020-06-28 12:02:13 +0900

    使ってない関数を消した

--- template.py
+++ template.py
@@ -11,20 +11,9 @@ def input():
 def get_nums_l():
     return [ int(s) for s in input().split(" ")]
 
-# 改行区切りの入力をn行読み込んで数値リストにして返します。
-def get_nums_n(n):
-    return [ int(input()) for _ in range(n)]
-
 # 改行またはスペース区切りの入力をすべて読み込んでイテレータを返します。
 def get_all_int():
     return map(int, open(0).read().split())
-
-def rangeI(it, l, r):
-    for i, e in enumerate(it):
-        if l <= i < r:
-            yield e
-        elif l >= r:
-            break
 
 def log(*args):
     print("DEBUG:", *args, file=sys.stderr)
