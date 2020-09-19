896fa05ffdcf47a02aaf71f8af6c4db160df7b21
むいにゃん <hngrmi@gmail.com>
2020-09-19 23:20:55 +0900

    ABC179 A-E

---
+++ abc/abc179/A.py
@@ -0,0 +1,31 @@
+import math
+from functools import reduce
+from collections import deque
+import sys
+sys.setrecursionlimit(10**7)
+
+def input():
+    return sys.stdin.readline().strip()
+
+# スペース区切りの入力を読み込んで数値リストにして返します。
+def get_nums_l():
+    return [ int(s) for s in input().split(" ")]
+
+# 改行またはスペース区切りの入力をすべて読み込んでイテレータを返します。
+def get_all_int():
+    return map(int, open(0).read().split())
+
+def log(*args):
+    print("DEBUG:", *args, file=sys.stderr)
+
+def main():
+    s = input()
+
+    if s.endswith("s"):
+        s += "es"
+    else:
+        s += "s"
+    
+    print(s)
+
+main()
