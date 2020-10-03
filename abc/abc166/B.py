7fdbf7638e7a7548bb318778848b471eca6260e8
むいにゃん <hngrmi@gmail.com>
2020-10-03 20:13:25 +0900

    ABC166 A-F

---
+++ abc/abc166/B.py
@@ -0,0 +1,50 @@
+import math
+from functools import reduce
+from collections import deque
+import sys
+sys.setrecursionlimit(10**7)
+
+def input():
+    return sys.stdin.readline().strip()
+
+# スペース区切りの入力を読み込んで数値リストにして返します。
+def get_nums_l():
+    return [ int(s) for s in input().split(" ")]
+
+# 改行区切りの入力をn行読み込んで数値リストにして返します。
+def get_nums_n(n):
+    return [ int(input()) for _ in range(n)]
+
+# 改行またはスペース区切りの入力をすべて読み込んでイテレータを返します。
+def get_all_int():
+    return map(int, open(0).read().split())
+
+def rangeI(it, l, r):
+    for i, e in enumerate(it):
+        if l <= i < r:
+            yield e
+        elif l >= r:
+            break
+
+def log(*args):
+    print("DEBUG:", *args, file=sys.stderr)
+
+INF = 999999999999999999999999
+MOD = 10**9+7
+
+n,k = get_nums_l()
+
+check = [False] * (n+1)
+
+for i in range(k):
+    d = int(input())
+    for a in get_nums_l():
+        check[a] = True
+
+ans = 0
+
+for i,b in enumerate(check):
+    if not b:
+        ans += 1
+
+print(ans-1)
