2d426b610c1822a91af972af21fd07cbb6484062
むいにゃん <hngrmi@gmail.com>
2020-10-10 12:27:59 +0900

    素因数分解から約数リスト

--- lib.py
+++ lib.py
@@ -92,6 +92,31 @@ def init_primes(MAX):
 
     return primes
 
+def divisors_from_prime_factors(prime_factors, need_sort=True):
+    """
+    素因数分解から約数リストを生成します。(1とその数自身を含む)
+
+    Parameters
+    ---
+    prime_factors
+        素因数分解を表現した、タプルのリスト。
+        p1^a1 * p2^a2 であれば、 [(p1,a1),(p2,a2)] 。
+    need_sort
+        Trueの場合、約数リストをソートして返します。
+    """
+    # 既知の約数リスト
+    div = [1]
+    for p,a in prime_factors:
+        # 既知の約数それぞれに対して、
+        # p^1倍, p^2倍, ... p^a倍 したものを計算して約数リストに追加する
+        m = len(div)
+        for i in range(m):
+            for j in range(1, a+1):
+                div.append(div[i] * p**j)
+    if need_sort:
+        div.sort()
+    return div
+
 class cumulative_sum():
     def __init__(self, array, key=lambda a: a):
         n = len(array)
