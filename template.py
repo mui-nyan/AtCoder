247b8738161469ed49eb1f2082bfe8122f95257b
むいにゃん <hngrmi@gmail.com>
2021-05-23 17:57:10 +0900

    テンプレート改変

--- template.py
+++ template.py
@@ -1,27 +1,23 @@
 import math
 from functools import reduce
-from collections import deque
+from collections import deque, defaultdict, Counter
 import sys
 sys.setrecursionlimit(10**7)
 
-def input():
-    return sys.stdin.readline().strip()
-
-# スペース区切りの入力を読み込んで数値リストにして返します。
-def get_nums_l():
-    return [ int(s) for s in input().split(" ")]
-
-# 改行またはスペース区切りの入力をすべて読み込んでイテレータを返します。
-def get_all_int():
-    return map(int, open(0).read().split())
-
-def log(*args):
-    print("DEBUG:", *args, file=sys.stderr)
+def input(): return sys.stdin.readline().strip()
+def INT():   return int(input())
+def INTS():  return [ int(s) for s in input().split(" ")]
+def GET(*types):
+    strs = input().split()
+    return [ t(s) for s,t in zip(strs, types)]
+def get_nums_l():  return [ int(s) for s in input().split(" ")]
+def get_all_int(): return map(int, open(0).read().split())
+def log(*args): print("DEBUG:", *args, file=sys.stderr)
 
 def main():
     INF = 999999999999999999999999
     MOD = 10**9+7
 
-    n,k,*A = get_all_int()
+    n, m = INTS()
 
 main()
