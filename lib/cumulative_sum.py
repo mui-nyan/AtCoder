f877d43b437ce9e2f6c9b9868ab9f91fd7241db8
むいにゃん <hngrmi@gmail.com>
2021-05-24 22:18:28 +0900

    演算を指定できるようにした

--- lib/cumulative_sum.py
+++ lib/cumulative_sum.py
@@ -1,9 +1,9 @@
-class cumulative_sum():
-    def __init__(self, array, key=lambda a: a):
+class CumulativeSum():
+    def __init__(self, array, key=lambda a: a, operation=lambda a,b: a+b):
         n = len(array)
         self.array = [0] * (n+1)
         for i,a in enumerate(array):
-            self.array[i+1] = self.array[i] + key(a)
+            self.array[i+1] = operation(self.array[i],key(a))
 
     def get(self, l, r):
         """指定した区間(半開区間)の合計を計算します。"""
