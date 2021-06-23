59bfb0f9a279e944a32b728aca5fad67fce53541
むいにゃん <hngrmi@gmail.com>
2021-06-23 20:42:03 +0900

    典型90 20

---
+++ others/typical90/20.py
@@ -0,0 +1,12 @@
+import sys
+sys.setrecursionlimit(10**7)
+
+def input(): return sys.stdin.readline().strip()
+def INTS():  return [ int(s) for s in input().split(" ")]
+def log(*args, **kwargs): print("DEBUG:", *args, **kwargs, file=sys.stderr)
+
+def main():
+    a,b,c = INTS()
+    print("Yes" if a < c**b else "No")
+
+main()
