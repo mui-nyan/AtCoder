b4eda0899af362f54414aa99252773189016c53c
むいにゃん <hngrmi@gmail.com>
2021-06-23 21:16:17 +0900

    典型90 21

---
+++ others/typical90/21.py
@@ -0,0 +1,76 @@
+from collections import Counter
+import sys
+sys.setrecursionlimit(10**7)
+
+def input(): return sys.stdin.readline().strip()
+def INTS():  return [ int(s) for s in input().split(" ")]
+def log(*args, **kwargs): print("DEBUG:", *args, **kwargs, file=sys.stderr)
+
+# 強連結成分分解(SCC): グラフGに対するSCCを行う
+# 入力: <N>: 頂点サイズ, <G>: 順方向の有向グラフ, <RG>: 逆方向の有向グラフ
+# 出力: (<ラベル数>, <各頂点のラベル番号>)
+def scc(N, G, RG):
+    order = []
+    used = [0]*N
+    group = [None]*N
+    def dfs(s):
+        used[s] = 1
+        for t in G[s]:
+            if not used[t]:
+                dfs(t)
+        order.append(s)
+    def rdfs(s, col):
+        group[s] = col
+        used[s] = 1
+        for t in RG[s]:
+            if not used[t]:
+                rdfs(t, col)
+    for i in range(N):
+        if not used[i]:
+            dfs(i)
+    used = [0]*N
+    label = 0
+    for s in reversed(order):
+        if not used[s]:
+            rdfs(s, label)
+            label += 1
+    return label, group
+
+# 縮約後のグラフを構築
+def construct(N, G, label, group):
+    G0 = [set() for i in range(label)]
+    GP = [[] for i in range(label)]
+    for v in range(N):
+        lbs = group[v]
+        for w in G[v]:
+            lbt = group[w]
+            if lbs == lbt:
+                continue
+            G0[lbs].add(lbt)
+        GP[lbs].append(v)
+    return G0, GP
+
+def main():
+    INF = 2**31-1
+    MOD = 10**9+7
+
+    n, m = INTS()
+    edges = [ [] for _ in range(n) ]
+    reverse_edges = [ [] for _ in range(n) ]
+    for i in range(m):
+        a,b = INTS()
+        a-=1
+        b-=1
+        edges[a].append(b)
+        reverse_edges[b].append(a)
+    
+    label, group = scc(n, edges, reverse_edges)
+    count = Counter(group)
+
+    ans = 0
+    for _,c in count.items():
+        ans += c * (c-1) // 2
+
+    print(ans)
+
+main()
