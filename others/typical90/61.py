60354f32f1d516f76c8c56635cc84048a0a490ba
むいにゃん <hngrmi@gmail.com>
2021-06-30 22:10:12 +0900

    典型90 61

---
+++ others/typical90/61.py
@@ -0,0 +1,78 @@
+import sys
+sys.setrecursionlimit(10**7)
+
+def input(): return sys.stdin.readline().strip()
+def INT():   return int(input())
+def INTS():  return [ int(s) for s in input().split(" ")]
+def log(*args, **kwargs): print("DEBUG:", *args, **kwargs, file=sys.stderr)
+
+def main():
+    deck = RandomAccessDeque()
+
+    q = INT()
+    for _ in range(q):
+        t, x = INTS()
+
+        if t == 1:
+            deck.appendleft(x)
+        elif t == 2:
+            deck.append(x)
+        else:
+            print(deck[x-1])
+
+class RandomAccessDeque:
+    # Pythonで各要素にO(1)でランダムアクセスできるdeque(両端キュー)を書いてみた
+    # https://prd-xxx.hateblo.jp/entry/2020/02/07/114818
+    def __init__(self, src_arr=[], max_size=300000):
+        self.N = max(max_size, len(src_arr)) + 1
+        self.buf = list(src_arr) + [None] * (self.N - len(src_arr))
+        self.head = 0
+        self.tail = len(src_arr)
+    def __index(self, i):
+        l = len(self)
+        if not -l <= i < l: raise IndexError('index out of range: ' + str(i))
+        if i < 0:
+            i += l
+        return (self.head + i) % self.N
+    def __extend(self):
+        ex = self.N - 1
+        self.buf[self.tail+1 : self.tail+1] = [None] * ex
+        self.N = len(self.buf)
+        if self.head > 0:
+            self.head += ex
+    def is_full(self):
+        return len(self) >= self.N - 1
+    def is_empty(self):
+        return len(self) == 0
+    def append(self, x):
+        if self.is_full(): self.__extend()
+        self.buf[self.tail] = x
+        self.tail += 1
+        self.tail %= self.N
+    def appendleft(self, x):
+        if self.is_full(): self.__extend()
+        self.buf[(self.head - 1) % self.N] = x
+        self.head -= 1
+        self.head %= self.N
+    def pop(self):
+        if self.is_empty(): raise IndexError('pop() when buffer is empty')
+        ret = self.buf[(self.tail - 1) % self.N]
+        self.tail -= 1
+        self.tail %= self.N
+        return ret
+    def popleft(self):
+        if self.is_empty(): raise IndexError('popleft() when buffer is empty')
+        ret = self.buf[self.head]
+        self.head += 1
+        self.head %= self.N
+        return ret
+    def __len__(self):
+        return (self.tail - self.head) % self.N
+    def __getitem__(self, key):
+        return self.buf[self.__index(key)]
+    def __setitem__(self, key, value):
+        self.buf[self.__index(key)] = value
+    def __str__(self):
+        return 'Deque({0})'.format(str(list(self)))
+
+main()
